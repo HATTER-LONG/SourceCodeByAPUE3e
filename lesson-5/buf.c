47111c2850fc73d9cef35c6fc070cf247e5a5b2d
HATTER-LONG <caolei6767@gmail.com>
2020-06-24 22:54:52 +0800

    【修改原因】修改第五章代码

--- lesson-5/buf.c
+++ lesson-5/buf.c
@@ -1,44 +1,42 @@
 #include "apue.h"
 
-void	pr_stdio(const char *, FILE *);
-int		is_unbuffered(FILE *);
-int		is_linebuffered(FILE *);
-int		buffer_size(FILE *);
+void pr_stdio(const char*, FILE*);
+int is_unbuffered(FILE*);
+int is_linebuffered(FILE*);
+int buffer_size(FILE*);
 
-int
-main(void)
+int main(void)
 {
-	FILE	*fp;
-	
-	
-	fputs("enter any character\n", stdout);
-	if (getchar() == EOF)
-		err_sys("getchar error");
-	fputs("one line to standard error\n", stderr);
+    FILE* fp;
 
-	pr_stdio("stdin",  stdin);
-	pr_stdio("stdout", stdout);
-	pr_stdio("stderr", stderr);
 
-	if ((fp = fopen("/etc/passwd", "r")) == NULL)
-		err_sys("fopen error");
-	if (getc(fp) == EOF)
-		err_sys("getc error");
-	pr_stdio("/etc/passwd", fp);
-	exit(0);
+    fputs("enter any character\n", stdout);
+    if (getchar() == EOF)
+        err_sys("getchar error");
+    fputs("one line to standard error\n", stderr);
+
+    pr_stdio("stdin", stdin);
+    pr_stdio("stdout", stdout);
+    pr_stdio("stderr", stderr);
+
+    if ((fp = fopen("/etc/passwd", "r")) == NULL)
+        err_sys("fopen error");
+    if (getc(fp) == EOF)
+        err_sys("getc error");
+    pr_stdio("/etc/passwd", fp);
+    exit(0);
 }
 
-void
-pr_stdio(const char *name, FILE *fp)
+void pr_stdio(const char* name, FILE* fp)
 {
-	printf("stream = %s, ", name);
-	if (is_unbuffered(fp))
-		printf("unbuffered");
-	else if (is_linebuffered(fp))
-		printf("line buffered");
-	else /* if neither of above */
-		printf("fully buffered");
-	printf(", buffer size = %d\n", buffer_size(fp));
+    printf("stream = %s, ", name);
+    if (is_unbuffered(fp))
+        printf("unbuffered");
+    else if (is_linebuffered(fp))
+        printf("line buffered");
+    else /* if neither of above */
+        printf("fully buffered");
+    printf(", buffer size = %d\n", buffer_size(fp));
 }
 
 /*
@@ -47,45 +45,27 @@ pr_stdio(const char *name, FILE *fp)
 
 #if defined(_IO_UNBUFFERED)
 
-int
-is_unbuffered(FILE *fp)
+int is_unbuffered(FILE* fp)
 {
-	printf("_UNIO_BUFFERE = %d , _IO_LINE_BUF = %d \n",_IO_UNBUFFERED,_IO_LINE_BUF);
-	return(fp->_flags & _IO_UNBUFFERED);
+    printf("_UNIO_BUFFERE = %d , _IO_LINE_BUF = %d \n", _IO_UNBUFFERED, _IO_LINE_BUF);
+    return (fp->_flags & _IO_UNBUFFERED);
 }
 
-int
-is_linebuffered(FILE *fp)
-{
-	return(fp->_flags & _IO_LINE_BUF);
-}
+int is_linebuffered(FILE* fp) { return (fp->_flags & _IO_LINE_BUF); }
 
-int
-buffer_size(FILE *fp)
-{
-	return(fp->_IO_buf_end - fp->_IO_buf_base);
-}
+int buffer_size(FILE* fp) { return (fp->_IO_buf_end - fp->_IO_buf_base); }
 
 #elif defined(__SNBF)
 
-int
-is_unbuffered(FILE *fp)
+int is_unbuffered(FILE* fp)
 {
-	printf("__SNBF = %d ,__SLBF = %d \n ",__SNBF , __SLBF);
-	return(fp->_flags & __SNBF);
+    printf("__SNBF = %d ,__SLBF = %d \n ", __SNBF, __SLBF);
+    return (fp->_flags & __SNBF);
 }
 
-int
-is_linebuffered(FILE *fp)
-{
-	return(fp->_flags & __SLBF);
-}
+int is_linebuffered(FILE* fp) { return (fp->_flags & __SLBF); }
 
-int
-buffer_size(FILE *fp)
-{
-	return(fp->_bf._size);
-}
+int buffer_size(FILE* fp) { return (fp->_bf._size); }
 
 #elif defined(_IONBF)
 
@@ -95,27 +75,21 @@ buffer_size(FILE *fp)
 #define _base __pad[2]
 #endif
 
-int
-is_unbuffered(FILE *fp)
+int is_unbuffered(FILE* fp)
 {
-        printf("_IONBF = %d ,_IOLBF = %d \n ",_IONBF , _IOLBF);
+    printf("_IONBF = %d ,_IOLBF = %d \n ", _IONBF, _IOLBF);
 
-	return(fp->_flag & _IONBF);
+    return (fp->_flag & _IONBF);
 }
 
-int
-is_linebuffered(FILE *fp)
-{
-	return(fp->_flag & _IOLBF);
-}
+int is_linebuffered(FILE* fp) { return (fp->_flag & _IOLBF); }
 
-int
-buffer_size(FILE *fp)
+int buffer_size(FILE* fp)
 {
 #ifdef _LP64
-	return(fp->_base - fp->_ptr);
+    return (fp->_base - fp->_ptr);
 #else
-	return(BUFSIZ);	/* just a guess */
+    return (BUFSIZ); /* just a guess */
 #endif
 }
 
@@ -124,4 +98,3 @@ buffer_size(FILE *fp)
 #error unknown stdio implementation!
 
 #endif
-
