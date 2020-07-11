9bedea321182942e3db7844c25f62b96ebc4e0d0
HATTER-LONG <caolei6767@gmail.com>
2020-07-11 11:28:32 +0800

    【修改原因】修改第九章代码

--- lesson-09/proc_team.c
+++ lesson-09/proc_team.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(void){
-	pid_t id = fork();
+int main(void)
+{
+    pid_t id = fork();
 
-	if(id < 0){
-		printf("fork error!!");
-		return -1;
-	}
-	else if( id == 0){
-		printf("child id is :%d\n",getpid());
-		while(1){
-			printf("child\n");
-			sleep(2);
-		}
-	}
-	else{
-		printf("father id is :%d\n",getpid());
-		while(1){
-			printf("father!\n");
-			sleep(2);
-		}
-	}
-	return 0;
+    if (id < 0)
+    {
+        printf("fork error!!");
+        return -1;
+    }
+    else if (id == 0)
+    {
+        printf("child id is :%d\n", getpid());
+        while (1)
+        {
+            printf("child\n");
+            sleep(2);
+        }
+    }
+    else
+    {
+        printf("father id is :%d\n", getpid());
+        while (1)
+        {
+            printf("father!\n");
+            sleep(2);
+        }
+    }
+    return 0;
 }
