4f8ae9441400d53664c4bffd69d49f8bb12f7cc3
HATTER-LONG <caolei6767@gmail.com>
2020-09-12 17:48:17 +0800

    【修改原因】修改第十六章笔记相关代码细节

--- lesson-16/big_small_data.c
+++ lesson-16/big_small_data.c
@@ -1,22 +1,16 @@
-/*************************************************************************
-	> File Name: big_small_data.c
-	> Author: Hatter
-	> Mail: caolei67674@gmail.com 
-	> Created Time: Mon 14 Nov 2016 08:58:28 AM CST
- ************************************************************************/
-
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
 
-int main(void){
-	uint32_t	i = 0x04030201;
-	unsigned char	*cp = (unsigned char *)&i;
+int main(void)
+{
+    uint32_t i = 0x04030201;
+    unsigned char* cp = (unsigned char*)&i;
 
-	if(*cp == 1)
-		printf("little-endian\n");
-	else if(*cp == 4)
-		printf("big-endian\n");
+    if (*cp == 1)
+        printf("little-endian\n");
+    else if (*cp == 4)
+        printf("big-endian\n");
 
-	return 0;
+    return 0;
 }
--- lesson-16/myhead.h
+++ lesson-16/myhead.h
@@ -1,9 +1,1 @@
-/*************************************************************************
-	> File Name: myhead.h
-	> Author: Hatter
-	> Mail: caolei67674@gmail.com 
-	> Created Time: Thu 10 Nov 2016 02:07:29 PM CST
- ************************************************************************/
-
-
-int connect_retry(int, int,...);
+int connect_retry(int, int, ...);
